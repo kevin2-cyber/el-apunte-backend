123f8e489eeae7baa15ec72ceade29aaba00f909
Kelvin Eduful <keduful@st.ug.edu.gh>
2026-04-24 17:12:48 +0000

    initialize notes-backend rest

---
+++ el-apunte-rest/src/test/java/com/kimikevin/el_apunte_rest/ElApunteRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimikevin.el_apunte_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElApunteRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
